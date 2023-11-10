26f01a24db8a45e76efed8746e1362d7e0f9113f
denerbatista <denerbatista@icloud.com>
2023-11-09 21:52:19 -0300

    feat: add style partial page login

--- src/components/BoxWhiteLogin/index.tsx
+++ src/components/BoxWhiteLogin/index.tsx
@@ -1,7 +1,25 @@
 import "./style.css";
 
 const BoxWhiteLogin = () => {
-  return <div id="boxWhiteLogin"></div>;
+  return (
+    <div id="boxWhiteLogin">
+      <h1 id="title-app">Agendamento de Ambientes Educacionais</h1>
+      <h2 id="title-login">Login</h2>
+      <div id="boxWhiteLogin-gradient">
+        <img
+          id="art"
+          src="public\art.png"
+          alt="mulher com computador olhando calendário"
+        />
+      </div>
+      <img id="logo-senai" src="public\logo-senai.png" alt="logomarca Senai" />
+      <img
+        id="art-mobile"
+        src="public\art-mobile.png"
+        alt="mulher com computador olhando calendário"
+      />
+    </div>
+  );
 };
 
 export default BoxWhiteLogin;
